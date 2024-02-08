b973fbf850b7f26810741231d95cf168fd2feca1
matheus <matheus.info@mazatarraf.com.br>
2024-02-08 09:05:53 -0300

    Bloco de Exercicios finalizados

--- 006 - Exercicios/002 - Tabuada/index.js
+++ 006 - Exercicios/002 - Tabuada/index.js
@@ -11,3 +11,15 @@
 
   Obs.: não podemos criar varios consoles.log(multiplicador x 3 = resultado)
 */
+
+const multiplicador = 30;
+
+//let i=0
+
+//significa que vai de zero a 10 apenas
+// ele add + 1 sobre o valor
+for(let i = 0; i <= 10; i++){
+    const calc = multiplicador * i;
+    //o ${} é um template literal, assim ele vai entender que isso é uma variavel e não uma String.
+    console.log(`${multiplicador} x ${i} = ${calc}`);
+}
