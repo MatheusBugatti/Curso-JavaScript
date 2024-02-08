a7f9402da6e105d4c58ed9e84804f44d6db0435e
matheus <matheus.info@mazatarraf.com.br>
2024-02-08 07:58:41 -0300

    Adicionado novo exercicio de tabuada

---
+++ 006 - Exercicios/002 - Tabuada/index.js
@@ -0,0 +1,13 @@
+/*
+  Precisamos mostrar a tabuada no console de 0 até 10 neste seguinte formato:
+
+  multiplicador x número = resultado
+  multiplicador x 0 = resultado
+  multiplicador x 1 = resultado
+  multiplicador x 2 = resultado
+  multiplicador x 3 = resultado
+  ...
+  multiplicador x 10 = resultado
+
+  Obs.: não podemos criar varios consoles.log(multiplicador x 3 = resultado)
+*/
